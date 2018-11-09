58e96c8d79fc95deb20a958fc947239920878464
kanapart <nivaask@gmail.com>
2018-11-08 22:08:20 -0600

    This is my first commit

---
+++ Python_exercises/test1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+name = "Srinivas"
+office = "intel"
+location = "Austin"
+job = "Software_Engineer"
+salary =100
+
+print ("Employee name is ",name, "staying in ",location, "working for",office,"his job title is",job)
+
+print ("Employee name is {0} staying in {1} working for {2} his job title is {3}" .format(name,office,location,job))
+
+print("Employee works for {0} and he lives in {1}." .format(office,location))
+
+print ("Employee name is %s working for %s staying in %s his job title is %s his salary is %d" %(name,office,location,job,salary))
+
+print ("Employee name is {0} staying in {1} working for {2} his job title is {3}" .format(name,office,location,job))
+
+print("Employee works for {0} and he lives in {1}." .format(office,location))
+
+'''
+print ("Employee name is %s working for %s staying in %s his job title is %s his salary is %d" %(name,office,location,job,salary))
+print ("Employee name is ",name, "staying in ",location, "working for",office,"his job title is",job)
+print ("Employee name is {0} staying in {1} working for {2} his job title is {3}" .format(name,office,location,job))
+print("Employee works for {0} and he lives in {1}." .format(office,location))
+print ("Employee name is %s working for %s staying in %s his job title is %s his salary is %d" %(name,office,location,job,salary))
+'''
+
+#print("Employee works for {0} and he lives in {1}." .format(office,location))
